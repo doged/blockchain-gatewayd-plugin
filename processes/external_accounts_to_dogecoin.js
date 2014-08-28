2cf467a6907989133e64ee683e282fa7be05ec7d
Steven Zeiler <zeiler.steven@gmail.com>
2014-08-28 05:07:27 +0000

    [TASK] Update accounts process to use new monitor module.

--- processes/external_accounts_to_dogecoin.js
+++ processes/external_accounts_to_dogecoin.js
@@ -1,16 +1,8 @@
-const request = require("superagent");
-const config = require(__dirname+'/../config/config.js');
 const gatewayd = require(__dirname+'/../../gatewayd/');
 const QueueWorker = require('sql-mq-worker');
 const blockchain =  require('blockchain-monitor');
 
-const blockchainClient = new blockchain.Client({
-
-});
-
-if (process.env.NODE_ENV != 'production') {
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-}
+const blockchainClient = new blockchain.Client(gatewayd.config.get('dogecoind'));
 
 const worker = new QueueWorker({
   Class: gatewayd.data.models.externalAccounts,
@@ -18,7 +10,7 @@ const worker = new QueueWorker({
     name: 'default'
   }},
   job: function(externalAccount, next) {
-    blockchainClient.getNewAddress(function(error, address) {
+    blockchainClient.coinDaemon.getnewaddress(function(error, address) {
       if (error) {
         console.log('ERROR', error);
         return next();
